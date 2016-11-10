b8e3de7d6a25dc8754928c2c4ba4e2c362bcb728
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-11-10 18:11:33 +0300

    ss

--- solar_input.py
+++ solar_input.py
@@ -23,6 +23,9 @@ def read_space_objects_data_from_file(input_filename):
                 star = Star()
                 parse_star_parameters(line, star)
                 objects.append(star)
+			elif object_type = "planet":
+				planet = Planet()
+				parse_planet_parameters(line, planet)
             else:
                 print("Unknown space object")
 
@@ -40,10 +43,18 @@ def parse_star_parameters(line, star):
 
     Параметры:
 
-    **line** — строка с описание звезды.
+    **line** — строка с описаниеm звезды.
     **star** — объект звезды.
     """
-
+    star.R = float(line.split()[1])
+    star.color = line.split()[2]
+    star.m = line.split()[3]
+    star.x = line.split()[4]
+    star.y = line.split()[5]
+    star.Vx = line.split()[6]
+    star.Vy = line.split()[7]
+		
+		
     pass  # FIXME: not done yet
 
 def parse_planet_parameters(line, planet):
@@ -61,6 +72,13 @@ def parse_planet_parameters(line, planet):
     **line** — строка с описание планеты.
     **planet** — объект планеты.
     """
+    planet.R = float(line.split()[1])
+    planet.color = line.split()[2]
+    planet.m = line.split()[3]
+    planet.x = line.split()[4]
+    planet.y = line.split()[5]
+    planet.Vx = line.split()[6]
+    planet.Vy = line.split()[7]
     pass  # FIXME: not done yet...
 
 
@@ -77,7 +95,9 @@ def write_space_objects_data_to_file(output_filename, space_objects):
     """
     with open(output_filename, 'w') as out_file:
         for obj in space_objects:
-            print(out_file, "%s %d %s %f" % ('1', 2, '3', 4.5))
+            out_file.write('{} {} {} {} {} {} {} {}\n'.format(\
+            	obj.type.capitalize(), obj.R, obj.color, obj.m, obj.x, obj.y, obj.Vx, obj.Vy))
+		out_file.close()
             # FIXME: should store real values
 
 # FIXME: хорошо бы ещё сделать функцию, сохранающую статистику в заданный файл...
